0d13debdc8928be4583b0ecb975379e0aaceff19
Necromnomicon <eladnotooh@gmail.com>
2021-03-24 16:03:27 -0500

    Update keymap.c

--- keyboards/preonic/keymaps/Necromnomicon/keymap.c
+++ keyboards/preonic/keymaps/Necromnomicon/keymap.c
@@ -13,7 +13,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-
+//making a simple change to see if is caught. 
 #include QMK_KEYBOARD_H
 
 
